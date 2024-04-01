0bc9303e2c0d6e76bfa58a320bcb8f74dc4c47e8
David Rodríguez <70174908+thedatadj@users.noreply.github.com>
2024-04-01 19:28:38 -0400

    added urls

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -9,24 +9,28 @@ const projects = [
     description:
       "We are a family owened Mediterranean restaurant, focused on traditional recipes served with a modern twist.",
     getImageSrc: () => require("../images/photo1.png"),
+    url: "https://thedatadj.github.io/littlelemon/"
   },
   {
     title: "React Infinite Scroll",
     description:
       "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
     getImageSrc: () => require("../images/photo2.jpg"),
+    url: ""
   },
   {
     title: "Photo Gallery",
     description:
       "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
     getImageSrc: () => require("../images/photo3.jpg"),
+    url: ""
   },
   {
     title: "Event planner",
     description:
       "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
     getImageSrc: () => require("../images/photo4.jpg"),
+    url: ""
   },
 ];
 
@@ -53,6 +57,7 @@ const ProjectsSection = () => {
             title={project.title}
             description={project.description}
             imageSrc={project.getImageSrc()}
+            url={project.url}
           />
         ))}
       </Box>
